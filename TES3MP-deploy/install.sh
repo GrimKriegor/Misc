4cd4b709de92811073c561360ae2ab0e3de67d12
GrimKriegor <grimkriegor@krutt.org>
2016-07-21 00:40:33 +0100

    New TES3MP repository path. Better path sanitization. String purrtyfication. Argument to set number of cores used in the build process.

--- TES3MP-deploy/install.sh
+++ TES3MP-deploy/install.sh
@@ -1,93 +1,97 @@
 #!/bin/bash
 
 #NUMBER OF CPU CORES USED FOR COMPILATION
-CORES=5
+if [ "$1" == "" ]; then
+    CORES=3
+else
+    CORES="$1"
+fi
 
 #DISTRO IDENTIFICATION
 DISTRO="$(lsb_release -si | awk '{print tolower($0)}')"
 
 #FOLDER HIERARCHY
 BASE="$(pwd)"
-CODE=$BASE/code
-DEVELOPMENT=$BASE/build
-KEEPERS=$BASE/keepers
-DEPENDENCIES=$BASE/dependencies
+CODE="$BASE/code"
+DEVELOPMENT="$BASE/build"
+KEEPERS="$BASE/keepers"
+DEPENDENCIES="$BASE/dependencies"
 
 #CREATE FOLDER HIERARCHY
-echo -e "\n\n\n>> Creating folder hierarchy"
-mkdir $DEVELOPMENT $KEEPERS $DEPENDENCIES
+echo -e ">> Creating folder hierarchy"
+mkdir "$DEVELOPMENT" "$KEEPERS" "$DEPENDENCIES"
 
 #CHECK DISTRO AND INSTALL DEPENDENCIES
-echo -e "\n\n\n>> Checking which GNU/Linux distro is installed"
+echo -e "\n>> Checking which GNU/Linux distro is installed"
 case $DISTRO in
   "arch" | "parabola" )
-      echo -e "\nYou seem to be running either Arch Linux or Parabola GNU/Linux-libre"
+      echo -e "You seem to be running either Arch Linux or Parabola GNU/Linux-libre"
       sudo pacman -Sy git cmake boost openal openscenegraph mygui bullet qt5-base ffmpeg sdl2 unshield libxkbcommon-x11 gcc-libs ;; #clang35 llvm35
 
   "debian" | "ubuntu" | "linuxmint" )
-      echo -e "\nYou seem to be running either Debian, Ubuntu or Mint"
+      echo -e "You seem to be running either Debian, Ubuntu or Mint"
       sudo apt-get update
       sudo apt-get install git libopenal-dev libopenscenegraph-dev libsdl2-dev libqt4-dev libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev libboost-system-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev libbullet-dev libmygui-dev libunshield-dev cmake build-essential libqt4-opengl-dev g++ ;; #llvm-3.5 clang-3.5 libclang-3.5-dev llvm-3.5-dev
 
   "fedora" )
-      echo -e "\nYou seem to be running Fedora"
+      echo -e "You seem to be running Fedora"
       sudo dnf --refresh groupinstall development-tools 
       sudo dnf --refresh install openal-devel OpenSceneGraph-qt-devel SDL2-devel qt4-devel boost-filesystem git boost-thread boost-program-options boost-system ffmpeg-devel ffmpeg-libs bullet-devel gcc-c++ mygui-devel unshield-devel tinyxml-devel cmake ;; #llvm35 llvm clang ncurses
 
   *)
-      echo -e "\n\n\nCould not determine your GNU/Linux distro, press any key to continue without installing dependencies"
+      echo -e "Could not determine your GNU/Linux distro, press any key to continue without installing dependencies"
       read ;;
 esac
     
 #PULL SOFTWARE VIA GIT
-echo -e "\n\n\n>> Downloading software"
-git clone https://github.com/TES3MP/TES3MP.git $CODE
-git clone https://github.com/OculusVR/RakNet.git $DEPENDENCIES/raknet
-#git clone https://github.com/zdevito/terra.git $DEPENDENCIES/terra
-wget https://github.com/zdevito/terra/releases/download/release-2016-02-26/terra-Linux-x86_64-2fa8d0a.zip -O $DEPENDENCIES/terra.zip
-git clone https://github.com/TES3MP/PluginExamples.git $KEEPERS/PluginExamples
+echo -e "\n>> Downloading software"
+git clone https://github.com/TES3MP/openmw-tes3mp.git "$CODE"
+git clone https://github.com/OculusVR/RakNet.git "$DEPENDENCIES"/raknet
+#git clone https://github.com/zdevito/terra.git "$DEPENDENCIES"/terra
+wget https://github.com/zdevito/terra/releases/download/release-2016-02-26/terra-Linux-x86_64-2fa8d0a.zip -O "$DEPENDENCIES"/terra.zip
+git clone https://github.com/TES3MP/PluginExamples.git "$KEEPERS"/PluginExamples
 
 #COPY STATIC SERVER AND CLIENT CONFIGS
-echo -e "\n\n\n>> Copying server and client configs to their permanent place"
-cp $CODE/files/tes3mp/tes3mp-{client,server}-default.cfg $KEEPERS
+echo -e "\n>> Copying server and client configs to their permanent place"
+cp "$CODE"/files/tes3mp/tes3mp-{client,server}-default.cfg "$KEEPERS"
 
 #SET home VARIABLE IN tes3mp-server-default.cfg
-echo -e "\n\n\n>> Autoconfiguring"
+echo -e "\n>> Autoconfiguring"
 sed -i "s|~/ClionProjects/PS-dev|$KEEPERS/PluginExamples|g" $KEEPERS/tes3mp-server-default.cfg
 
 #DIRTY HACKS
-echo -e "\n\n\n>> Applying some dirty hacks"
+echo -e "\n>> Applying some dirty hacks"
 sed -i "s|tes3mp.lua,chat_parser.lua|server.lua|g" $KEEPERS/tes3mp-server-default.cfg #Fixes server scripts
 sed -i "s|Y #key for switch chat mode enabled/hidden/disabled|Left Alt|g" $KEEPERS/tes3mp-client-default.cfg #Changes chat key to Left Alt
 sed -i "s|mp.tes3mp.com|grimkriegor.zalkeen.pw|g" $KEEPERS/tes3mp-client-default.cfg #Sets Grim's server as the default
 
 
 #BUILD RAKNET
-echo -e "\n\n\n>> Building RakNet"
-mkdir $DEPENDENCIES/raknet/build
-cd $DEPENDENCIES/raknet/build
+echo -e "\n>> Building RakNet"
+mkdir "$DEPENDENCIES"/raknet/build
+cd "$DEPENDENCIES"/raknet/build
 cmake -DCMAKE_BUILD_TYPE=Release -DRAKNET_ENABLE_DLL=OFF -DRAKNET_ENABLE_SAMPLES=OFF -DRAKNET_ENABLE_STATIC=ON -DRAKNET_GENERATE_INCLUDE_ONLY_DIR=ON ..
 make -j$CORES
 
-cd $BASE
+cd "$BASE"
 
 ##BUILD TERRA
-#echo -e "\n\n\n>> Building Terra"
+#echo -e "\n>> Building Terra"
 #cd $DEPENDENCIES/terra/
 #make -j$CORES
 
 #UNPACK AND PREPARE TERRA
-echo -e "\n\n\n>> Unpacking and preparing Terra"
-cd $DEPENDENCIES
+echo -e "\n>> Unpacking and preparing Terra"
+cd "$DEPENDENCIES"
 unzip terra.zip
 mv terra-* terra
 rm terra.zip
 
-cd $BASE
+cd "$BASE"
 
 #CALL upgrade.sh TO BUILD TES3MP
-echo -e "\n\n\nPreparing to build TES3MP"
-bash upgrade.sh --install
+echo -e "\n>>Preparing to build TES3MP"
+bash upgrade.sh "$CORES" --install
 
 read
 
--- TES3MP-deploy/upgrade.sh
+++ TES3MP-deploy/upgrade.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
 #NUMBER OF CPU CORES USED FOR COMPILATION
-CORES=5
-
-#DISTRO IDENTIFICATION
-DISTO="$(lsb_release -si)"
+if [ "$1" == "" ]; then
+    CORES=3
+else
+    CORES="$1"
+fi
 
 #FOLDER HIERARCHY
 BASE="$(pwd)"
-CODE=$BASE/code
-DEVELOPMENT=$BASE/build
-KEEPERS=$BASE/keepers
-DEPENDENCIES=$BASE/dependencies
+CODE="$BASE/code"
+DEVELOPMENT="$BASE/build"
+KEEPERS="$BASE/keepers"
+DEPENDENCIES="$BASE/dependencies"
 
 #LOCATIONS OF RAKNET AND TERRA
-RAKNET_LOCATION="$DEPENDENCIES/raknet"
-TERRA_LOCATION="$DEPENDENCIES/terra"
+RAKNET_LOCATION="$DEPENDENCIES"/raknet
+TERRA_LOCATION="$DEPENDENCIES"/terra
 
 #PULL CODE CHANGES FROM GIT
-echo -e "\n\n\n>> Pulling code changes from git"
-cd $CODE
+echo -e "\n>> Pulling code changes from git"
+cd "$CODE"
 git pull
-cd $BASE
+cd "$BASE"
 
 #OPTION TO UPGRADE AFTER PULLING THE CHANGES FROM THE GIT REPOSITORY
-if [ "$1" = "--install" ]; then
+if [ "$2" = "--install" ]; then
   UPGRADE="YES"
 else
-  echo -e "\n\nDo you wish the upgrade TES3MP? (type YES to continue)"
+  echo -e "\nDo you wish the upgrade TES3MP? (type YES to continue)"
   read UPGRADE
 fi
 
 #REBUILD OPENMW/TES3MP
 if [ "$UPGRADE" = "YES" ]; then
-  echo -e "\n\n\n>> Doing a clean build of TES3MP"
+  echo -e "\n>> Doing a clean build of TES3MP"
 
-  rm -r $DEVELOPMENT
-  mkdir $DEVELOPMENT
+  rm -r "$DEVELOPMENT"
+  mkdir "$DEVELOPMENT"
 
-  cd $DEVELOPMENT
-  cmake $CODE -DBUILD_OPENCS=OFF -DRakNet_INCLUDES=${RAKNET_LOCATION}/include/RakNet -DRakNet_LIBRARY_DEBUG=${RAKNET_LOCATION}/build/Lib/LibStatic/libRakNetLibStatic.a -DRakNet_LIBRARY_RELEASE=${RAKNET_LOCATION}/build/Lib/LibStatic/libRakNetLibStatic.a -DTerra_LIBRARY_RELEASE=${TERRA_LOCATION}/lib/libterra.a -DTerra_INCLUDES=${TERRA_LOCATION}/include
+  cd "$DEVELOPMENT"
+  cmake "$CODE" -DBUILD_OPENCS=OFF -DRakNet_INCLUDES="${RAKNET_LOCATION}"/include/RakNet -DRakNet_LIBRARY_DEBUG="${RAKNET_LOCATION}"/build/Lib/LibStatic/libRakNetLibStatic.a -DRakNet_LIBRARY_RELEASE="${RAKNET_LOCATION}"/build/Lib/LibStatic/libRakNetLibStatic.a -DTerra_LIBRARY_RELEASE="${TERRA_LOCATION}"/lib/libterra.a -DTerra_INCLUDES="${TERRA_LOCATION}"/include
   make -j $CORES
 
 fi
 
 #CREATE SYMLINKS FOR THE CONFIG FILES INSIDE THE NEW BUILD FOLDER
-echo -e "\n\n\n>> Creating symlinks of the config files in the build folder"
-for file in $KEEPERS/*
+echo -e "\n>> Creating symlinks of the config files in the build folder"
+for file in "$KEEPERS"/*
 do
   FILEPATH=$file
   FILENAME=$(basename $file)
@@ -55,5 +56,5 @@ do
 done
 
 #ALL DONE
-echo -e "\n\n\nAll done! Press any key to exit."
+echo -e "\n\n\nAll done! Press any key to exit.\nMay Vehk bestow his blessing upon your Muatra."
 read

61641319b3af3c396bb0b9d42743f852471a8554
GrimKriegor <grimkriegor@krutt.org>
2017-04-25 06:23:25 +0100

    Small fixes

--- BatteryPlotter/batteryplotter.sh
+++ BatteryPlotter/batteryplotter.sh
@@ -9,12 +9,12 @@ LOG_FILE="battery.log"
 DEFAULT_INTERVAL=5
 
 PLOT_SCRIPT="\
-set title \"Battery capacity and computer load over time\"
+set title \"Battery capacity and system load over time\"
 set xlabel \"Time (minutes)\"
 set ylabel \"(%)\"
 set terminal png size 600,500 enhanced font \"Liberation Sans,9\"
 set output \"battery-graph.png\"
-plot for [col=2:3] \"battery.table\" using 1:col with lines title columnheader\
+plot for [col=2:3] \"battery.table\" using (\$1/60):col with lines title columnheader\
 "
 
 CORES=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
@@ -35,7 +35,7 @@ elif [ "$1" == "plot" ]; then
   exit 0
 
 elif [ "$1" == "clean" ]; then
-  echo "Time	Battery Capacity	Load" > battery.table
+  echo "Time	Battery	Load" > battery.table
   awk 'NR % 2 == 0' "$LOG_FILE" >> battery.table
   exit 0
 
